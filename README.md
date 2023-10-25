85c111f948d1cf1581876120a0025fa0d0e03ef5
LAPTOP-SVESTDUI\sylvia <sylviacooperhouse@gmail.com>
2023-10-25 11:28:06 -0700

    Mocha Bot 2.0

--- README.md
+++ README.md
@@ -1,38 +1,40 @@
-# Discord Puppy Mocha 1.0
+# Discord Puppy Mocha 2.0
 
 ## Project Description
 
-**Discord Doggo** is a straightforward and playful Discord chat bot designed to add some canine charm to your server. With only three interaction commands, it offers a user-friendly and uncomplicated way to make your Discord channel more engaging and enjoyable.
+**Discord Puppy Mocha 2.0** is an enhanced and delightful Discord chat bot that brings an even more sophisticated canine presence to your server. With an upgraded model and an extended range of features, this bot adds personality and engagement to your Discord community.
 
 ## Features
 
-- **Simplicity:** Discord Doggo offers just three interaction commands for ease of use.
-- **Playful Replies:** Trigger messages like "woo-hoo" receive cheerful responses like "woof woof!"
-- **Expresses Affection:** When users say "I love you," Discord Doggo replies with heartwarming messages like "I love you too, hooman!"
+- **Playful Interaction:** Engage with Mocha using three trigger messages for playful and heartwarming responses.
+- **Smart Responses:** Mocha offers clever replies and friendly greetings, making your Discord experience enjoyable.
+- **Intuitive Control:** Start a conversation with "come on mocha" and say "go rest mocha" to conclude the interaction.
+- **Enhanced Model:** Mocha 2.0 uses an upgraded natural language processing model for more intelligent and context-aware responses.
+- **Express Affection:** Mocha reciprocates love with responses like "I love you too, hooman!" and "I am the cutest!"
 
 ## Getting Started
 
 1. **Discord Bot Setup:**
-   - Create a bot on the Discord Developer Portal.
+   - Create your Discord bot on the Discord Developer Portal.
    - Obtain your bot's API token.
 
 2. **Installation:**
-   - Clone or download the Discord Doggo project.
+   - Clone or download the Discord Puppy Mocha 2.0 project.
 
 3. **Environment Variables:**
-   - Create a .env file with your Discord API token (e.g., `TOKEN=your-bot-token`).
+   - Create a `.env` file and add your Discord API token (e.g., `TOKEN=your-bot-token`).
 
 4. **Running the Bot:**
-   - Install dependencies.
+   - Install project dependencies.
    - Start the bot using Node.js.
-   - End the Baby Mocha bot Press Ctrl + C
+   - Use Ctrl + C to stop Mocha gracefully.
 
-5. **Engage:**
-   - Invite Discord Doggo to your server.
-   - Interact with the bot using the three available trigger messages and enjoy its playful responses.
+5. **Engage with Mocha:**
+   - Invite Discord Puppy Mocha to your server and witness the playful interactions.
+   - Use the available trigger messages and enjoy the clever and context-aware responses.
 
 ## Contribution and Customization
 
-Discord Doggo is open-source and easily customizable. Feel free to adapt it to your server's unique personality or contribute to its development.
+Discord Puppy Mocha 2.0 is an open-source project, designed to be easily customizable. Feel free to adapt it to your server's unique personality or contribute to its development.
 
-Enjoy some canine fun in your Discord server with Discord Doggo, a simple and delightful chat bot.
+Enjoy the delightful presence of Mocha 2.0, the upgraded Discord chat bot, in your server.
--- index.js
+++ index.js
@@ -1,28 +1,90 @@
-const { Client, GatewayIntentBits, Message } = require('discord.js')
-require('dotenv/config')
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const { exec } = require('child_process');
 
-const client = new Client({ 
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ] 
-})
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// Add a variable to track whether the bot should respond
+let shouldRespond = false;
 
 client.on('ready', () => {
-    console.log('The bot is ready')
-})
+  console.log('The bot is ready');
+});
 
-client.on('messageCreate', message => {
-    console.log(`Received message: "${message.content}"`);
-    if (message.content === 'woo-hoo') {
-      message.reply('woof woof!');
-    } else if (message.content === 'good baby') {
-      message.channel.send('Arf arf!');
-    } else if (message.content === 'I love you') {
-      message.channel.send('I love you too, hooman!');
+client.on('messageCreate', (message) => {
+  console.log(`Received message: "${message.content}"`);
+
+  // Check if the message was sent by the bot itself
+  if (message.author.bot) {
+    // Ignore messages from the bot
+    return;
+  }
+
+  const lowercaseMessage = message.content.toLowerCase();
+
+  // Check if the message includes the start keyword
+  if (lowercaseMessage.includes('come on mocha')) {
+    shouldRespond = true;
+    message.reply('Mocha is here! 🐶');
+    return; // Exit early to avoid additional processing
+  }
+
+  // Check if the message includes the stop keyword
+  if (lowercaseMessage.includes('go rest mocha')) {
+    shouldRespond = false;
+    message.reply('Ok! Love u!🐾');
+    return; // Exit early to avoid additional processing
+  }
+
+  // Continue processing the message if shouldRespond is true
+  if (shouldRespond) {
+    // Add logic to respond based on the analysis
+    if (message.content.includes('fetch')) {
+      message.reply('Sure, I can fetch! 🐾');
+    } else if (message.content.includes('play')) {
+      message.reply('I love to play! 🎾');
+    } else if (message.content.includes('love you')) {
+      message.reply('I love you too! ❤️');
+    } else if (message.content.includes('you\'re so cute')) {
+      message.reply('I am the cutest! 🐾');
+    } else if (message.content.includes('what\'s your name')) {
+      message.reply('I\'m Mocha, your loyal puppy companion! 🐶');
+    } else if (message.content.includes('food')) {
+      message.reply('Did someone say food? I\'m always hungry! 🍖');
+    } else if (message.content.includes('good dog')) {
+      message.reply('Arf arf! Thank you, hooman! 🐕');
+    } else if (message.content.includes('how are you')) {
+      message.reply('I\'m pawsitively great! How about you, hooman?');
+    } else if (message.content.includes('tell me a joke')) {
+      // You can add a function to fetch and send a random joke
+      sendRandomJoke(message.channel);
+    } else if (message.content.includes('sing a song')) {
+      // You can add a function to make the bot sing a song
+      singASong(message.channel);
+    } else {
+      // If no specific condition is met, respond with a default message
+      message.reply('Woof woof! 🐶');
     }
+  }
   });
-  
 
-client.login(process.env.TOKEN)
+// Function to send a random joke
+function sendRandomJoke(channel) {
+  // You can fetch a joke from an API or have a predefined list of jokes to send
+  // Send the joke to the specified channel
+  channel.send('Why did the dog sit in the shade? Because he didn\'t want to be a hot dog! 🌭');
+}
+
+// Function to make the bot sing a song
+function singASong(channel) {
+  // You can add logic to make the bot sing a song or send lyrics
+  channel.send('🎵 I\'m a little puppy, happy and spry, running around under the sky. 🎶');
+}
+
+client.login(token);
